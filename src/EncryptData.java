0b99ec5e05711650eb361bee23bb14363fa38178
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 19:37:26 +0200

    Update EncryptData.java
    In Ukrainian

--- src/EncryptData.java
+++ src/EncryptData.java
@@ -1,8 +1,8 @@
 import java.util.Base64;
 
 public interface EncryptData {
-    public default String encryptString(String data) { //метод для кодирования данных (стандарт кодирования двоичных
-        // данных при помощи только 64 символов ASCII. Алфавит кодирования содержит латинские символы A-Z, a-z, цифры 0-9)
+    public default String encryptString(String data) { // mетод для кодування даних (стандарт кодування двійкових
+        // даних за допомогою лише 64 символів ASCII. Алфавіт кодування містить латинські символи A-Z, a-z, цифри 0-9)
         String bufferForEncryptedData = Base64.getEncoder().encodeToString(data.getBytes());
         return bufferForEncryptedData;
     }
