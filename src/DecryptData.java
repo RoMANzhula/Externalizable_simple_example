5799adbbca255f9fb61c4b01f4a7ad93bb740dcb
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 19:36:43 +0200

    Update DecryptData.java
    In Ukrainian

--- src/DecryptData.java
+++ src/DecryptData.java
@@ -1,7 +1,7 @@
 import java.util.Base64;
 
 public interface DecryptData {
-    public default String decryptString(String data) { //метод для раскодирования данных
+    public default String decryptString(String data) { //метод для розкодування даних
         String bufferForDecryptedData = new String(Base64.getDecoder().decode(data));
         return bufferForDecryptedData;
     }
